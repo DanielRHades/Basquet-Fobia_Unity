0b658dd14b3d125828b2a70b6c73d9f6753027d1
Daniel Rodriguez <drodriguez442@unab.edu.co>
2024-11-10 18:23:41 -0500

    Avance_Sistema_Puntos

---
+++ Assets/Scripts/CestaDerechaTriggerP1.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class CestaDerechaTriggerP1 : MonoBehaviour
+{
+    private int contadorPuntosPlayer1 = 0;
+    private const int puntosParaGanar = 12;
+
+    // Referencia al TextMeshPro en pantalla para Player 1
+    public TextMeshProUGUI puntosPlayer1Text;
+
+    private void Start()
+    {
+        ActualizarTextoPuntos();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("BalonCancha"))
+        {
+            Debug.Log("Balón detectado en PuntoPlayer1. Tag del balón: " + other.tag);
+            contadorPuntosPlayer1 += 2;
+            Debug.Log("Puntos Jugador 1: " + contadorPuntosPlayer1);
+
+            ActualizarTextoPuntos();
+
+            if (contadorPuntosPlayer1 >= puntosParaGanar)
+            {
+                CambiarAEscenaGanador();
+            }
+        }
+    }
+
+    private void ActualizarTextoPuntos()
+    {
+        if (puntosPlayer1Text != null)
+        {
+            puntosPlayer1Text.text = "Jugador 1: " + contadorPuntosPlayer1.ToString();
+        }
+    }
+
+    private void CambiarAEscenaGanador()
+    {
+        SceneManager.LoadScene(3);
+    }
+}
---
+++ Assets/Scripts/CestaIzquierdaTriggerP2.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class CestaIzquierdaTriggerP2 : MonoBehaviour
+{
+    private int contadorPuntosPlayer2 = 0;
+    private const int puntosParaGanar = 12;
+
+    // Referencia al TextMeshPro en pantalla para Player 2
+    public TextMeshProUGUI puntosPlayer2Text;
+
+    private void Start()
+    {
+        ActualizarTextoPuntos();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("BalonCancha"))
+        {
+            Debug.Log("Balón detectado en PuntoPlayer2. Tag del balón: " + other.tag);
+            contadorPuntosPlayer2 += 2;
+            Debug.Log("Puntos Jugador 2: " + contadorPuntosPlayer2);
+
+            ActualizarTextoPuntos();
+
+            if (contadorPuntosPlayer2 >= puntosParaGanar)
+            {
+                CambiarAEscenaGanador();
+            }
+        }
+    }
+
+    private void ActualizarTextoPuntos()
+    {
+        if (puntosPlayer2Text != null)
+        {
+            puntosPlayer2Text.text = "Jugador 2: " + contadorPuntosPlayer2.ToString();
+        }
+    }
+
+    private void CambiarAEscenaGanador()
+    {
+        SceneManager.LoadScene(3);
+    }
+}
